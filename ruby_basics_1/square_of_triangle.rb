964578e09f82f57cbaa4e94b3849fa7cc3cc0332
Dmitry Zaguta <zdvbind@gmail.com>
2021-03-23 14:09:50 +0500

    area of a triangle

---
+++ ruby_basics_1/square_of_triangle.rb
@@ -0,0 +1,6 @@
+puts 'Введите высоту треугольника'
+hight = gets.chomp.to_f
+puts 'Введите длину основания треугольника'
+base = gets.chomp.to_f
+square = 0.5 * hight * base
+puts "Площадь треугольника равна #{square}"
