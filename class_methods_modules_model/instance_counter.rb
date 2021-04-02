97ba960a40aedc4140968d991269023b96333c06
Dmitry Zaguta <zdvbind@gmail.com>
2021-04-02 10:45:33 +0500

    fix getterin instance_counter

--- class_methods_modules_model/instance_counter.rb
+++ class_methods_modules_model/instance_counter.rb
@@ -5,14 +5,17 @@ module InstanceCounter
   end
 
   module ClassMethods
-    attr_accessor :instances
+    attr_writer :instances
+
+    def instances
+      @instances ||= 0
+    end
   end
 
   module InstanceMethods
     protected
 
     def register_instance
-      self.class.instances ||= 0
       self.class.instances += 1
     end
   end
